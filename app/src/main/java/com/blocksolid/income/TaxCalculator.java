39d4ed7d8fc3763feba290dc3bdb2467c00a4f8e
Dan Buckland <danbucklan@gmail.com>
2015-09-20 20:06:26 +0100

    Add comments to method for future sanity

--- app/src/main/java/com/blocksolid/income/TaxCalculator.java
+++ app/src/main/java/com/blocksolid/income/TaxCalculator.java
@@ -27,16 +27,32 @@ public class TaxCalculator {
     }
 
     public int calculatePersonalAllowance(int annualIncome) {
+
+        /**
+         * The standard Personal Allowance is £10,600, which is the amount of income that tax will
+         * not be paid on.
+         *
+         * A persons Personal Allowance may be bigger if they were born before 6 April 1938 or if
+         * they get Blind Person’s Allowance.
+         *
+         * The Personal Allowance goes down by £1 for every £2 that the adjusted net income is
+         * above £100,000. This means the allowance is zero if a persons income is £121,200 or over.
+         */
+
         if (annualIncome > PERSONAL_ALLOWANCE_THRESHOLD) {
             int difference = annualIncome - PERSONAL_ALLOWANCE_THRESHOLD;
+            // £1 is removed for each £2 earned above the personal allowance
+            // Remove the odd pound so that the difference can be divided by 2
             if (difference % 200 > 0) {
                 difference = difference - 100;
             }
             personalAllowance = MAX_PERSONAL_ALLOWANCE - (difference / 2);
+            // If the calculation results in a negative number, set personal allowance to 0
             if (personalAllowance < 0) {
                 personalAllowance = 0;
             }
         } else {
+            // Personal allowance set to the maximum if salary is below the threshold
             personalAllowance = MAX_PERSONAL_ALLOWANCE;
         }
         return personalAllowance;
