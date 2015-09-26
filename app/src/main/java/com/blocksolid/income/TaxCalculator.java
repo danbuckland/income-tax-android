3a9ebced58bc4f577101625820df6f836a63fe25
Dan Buckland <danbucklan@gmail.com>
2015-09-26 12:56:56 +0100

    Rename annualIncome to grossAnnualIncome in TaxCalculator class

--- app/src/main/java/com/blocksolid/income/TaxCalculator.java
+++ app/src/main/java/com/blocksolid/income/TaxCalculator.java
@@ -1,32 +1,38 @@
 package com.blocksolid.income;
 
-/**
- * Created by Dan Buckland on 16/09/2015.
- */
-
 public class TaxCalculator {
 
     int MAX_PERSONAL_ALLOWANCE = 1060000;           // £10,600
     int PERSONAL_ALLOWANCE_THRESHOLD = 10000000;    // £100,000
     int BASIC_RATE_THRESHOLD = 3178500;             // £31,785
     int HIGHER_RATE_THRESHOLD = 15000000;           // £150,000
+    double BASIC_RATE = 0.2;                        // 20%
+    double HIGHER_RATE = 0.4;                       // 40%
+    double ADDITIONAL_RATE = 0.45;                  // 45%
 
-    int annualIncome = 0;
+    int grossAnnualIncome = 0;
     int personalAllowance = MAX_PERSONAL_ALLOWANCE;
+    int basicRateDeduction = 0;
+    int higherRateDeduction = 0;
+    int additionalRateDeduction = 0;
+    int nationalInsuranceContribution = 0;
+    int totalDeductions = 0;
+    int netAnnualIncome = 0;
 
     public TaxCalculator() {
 
     }
 
-    public void setAnnualIncome(int newAnnualIncome) {
-        annualIncome = newAnnualIncome;
+    public void setGrossAnnualIncome(int newGrossAnnualIncome) {
+        grossAnnualIncome = newGrossAnnualIncome;
+        calculateTotalTaxDeductions(grossAnnualIncome);
     }
 
-    public int getAnnualIncome() {
-        return annualIncome;
+    public int getGrossAnnualIncome() {
+        return grossAnnualIncome;
     }
 
-    public int calculatePersonalAllowance(int annualIncome) {
+    public int calculatePersonalAllowance(int grossAnnualIncome) {
 
         /**
          * The standard Personal Allowance is £10,600, which is the amount of income that tax will
@@ -39,8 +45,8 @@ public class TaxCalculator {
          * above £100,000. This means the allowance is zero if a persons income is £121,200 or over.
          */
 
-        if (annualIncome > PERSONAL_ALLOWANCE_THRESHOLD) {
-            int difference = annualIncome - PERSONAL_ALLOWANCE_THRESHOLD;
+        if (grossAnnualIncome > PERSONAL_ALLOWANCE_THRESHOLD) {
+            int difference = grossAnnualIncome - PERSONAL_ALLOWANCE_THRESHOLD;
             // £1 is removed for each £2 earned above the personal allowance
             // Remove the odd pound so that the difference can be divided by 2
             if (difference % 200 > 0) {
--- app/src/test/java/com/blocksolid/income/TaxCalculatorTest.java
+++ app/src/test/java/com/blocksolid/income/TaxCalculatorTest.java
@@ -11,21 +11,21 @@ public class TaxCalculatorTest {
     // Tests for calculatePersonalAllowance method
 
     @Test
-    public void testPersonalAllowanceCalculationForAnnualIncomeOf0() throws Exception {
+    public void testPersonalAllowanceCalculationForGrossAnnualIncomeOf0() throws Exception {
         // The personal allowance for a salary of £0 is the maximum £10,600
         int result = taxCalculator.calculatePersonalAllowance(0);
         assertEquals(1060000, result);
     }
 
     @Test
-    public void testPersonalAllowanceCalculationForAnnualIncomeOf100001() throws Exception {
+    public void testPersonalAllowanceCalculationForGrossAnnualIncomeOf100001() throws Exception {
         // The personal allowance for a salary of £100,001 is the maximum £10,600
         int result = taxCalculator.calculatePersonalAllowance(10000100);
         assertEquals(1060000, result);
     }
 
     @Test
-    public void testPersonalAllowanceCalculationForAnnualIncomeOf100002() throws Exception {
+    public void testPersonalAllowanceCalculationForGrossAnnualIncomeOf100002() throws Exception {
         // £1 is removed from the personal allowance for every £2 over £100,000
         // The personal allowance for a salary of £100,002 should be £10,599
         int result = taxCalculator.calculatePersonalAllowance(10000200);
@@ -33,21 +33,21 @@ public class TaxCalculatorTest {
     }
 
     @Test
-    public void testPersonalAllowanceCalculationForAnnualIncomeOf121199() throws Exception {
+    public void testPersonalAllowanceCalculationForGrossAnnualIncomeOf121199() throws Exception {
         // The personal allowance for a salary of £121,199 should be £1
         int result = taxCalculator.calculatePersonalAllowance(12119900);
         assertEquals(100, result);
     }
 
     @Test
-    public void testPersonalAllowanceCalculationForAnnualIncomeOf121200() throws Exception {
+    public void testPersonalAllowanceCalculationForGrossAnnualIncomeOf121200() throws Exception {
         // There is no personal allowance for a salary of £121,200 or over
         int result = taxCalculator.calculatePersonalAllowance(12120000);
         assertEquals(0, result);
     }
 
     @Test
-    public void testPersonalAllowanceCalculationForAnnualIncomeOf1000000() throws Exception {
+    public void testPersonalAllowanceCalculationForGrossAnnualIncomeOf1000000() throws Exception {
         // There is no personal allowance for a salary of £1,000,000
         // This checks that it's not possible to have a negative personal allowance
         int result = taxCalculator.calculatePersonalAllowance(100000000);
@@ -55,7 +55,7 @@ public class TaxCalculatorTest {
     }
 
     @Test
-    public void testPersonalAllowanceCalculationForNegativeAnnualIncome() throws Exception {
+    public void testPersonalAllowanceCalculationForNegativeGrossAnnualIncome() throws Exception {
         // Checking that a negative annual income returns the maximum personal allowance
         int result = taxCalculator.calculatePersonalAllowance(-10500000);
         assertEquals(1060000, result);
@@ -64,14 +64,14 @@ public class TaxCalculatorTest {
     // Tests for calculateTotalTaxDeductions method
 
     @Test
-    public void testTaxDeductionCalculationForAnnualIncomeOf40000() throws Exception {
+    public void testTaxDeductionCalculationForGrossAnnualIncomeOf40000() throws Exception {
         // A salary of £40,000 should pay tax of £5,880
         int result = taxCalculator.calculateTotalTaxDeductions(4000000);
         assertEquals(588000, result);
     }
 
     @Test
-    public void testTaxDeductionCalculationForAnnualIncomeOf100000() throws Exception {
+    public void testTaxDeductionCalculationForGrossAnnualIncomeOf100000() throws Exception {
         // A salary of £100,000 should pay tax of £29,403
         //  £6,357 at the Basic rate 20%
         // £23,046 at the Higher rate 40%
@@ -80,7 +80,7 @@ public class TaxCalculatorTest {
     }
 
     @Test
-    public void testTaxDeductionCalculationForAnnualIncomeOf160000() throws Exception {
+    public void testTaxDeductionCalculationForGrossAnnualIncomeOf160000() throws Exception {
         // A salary of £160,000 should pay tax of £58,143
         //  £6,357 at the Basic rate 20%
         // £47,286 at the Higher rate 40%
@@ -90,7 +90,7 @@ public class TaxCalculatorTest {
     }
 
     @Test
-    public void testTaxDeductionCalculationForAnnualIncomeOf1000000() throws Exception {
+    public void testTaxDeductionCalculationForGrossAnnualIncomeOf1000000() throws Exception {
         // A salary of £1,000,000 should pay tax of £436,143
         //   £6,357 at the Basic rate 20%
         //  £47,286 at the Higher rate 40%
@@ -100,14 +100,14 @@ public class TaxCalculatorTest {
     }
 
     @Test
-    public void testTaxDeductionCalculationForAnnualIncomeOf0() throws Exception {
+    public void testTaxDeductionCalculationForGrossAnnualIncomeOf0() throws Exception {
         // A salary of £0 should pay no tax
         int result = taxCalculator.calculateTotalTaxDeductions(0);
         assertEquals(0, result);
     }
 
     @Test
-    public void testTaxDeductionCalculationForNegativeAnnualIncome() throws Exception {
+    public void testTaxDeductionCalculationForNegativeGrossAnnualIncome() throws Exception {
         // Checking that a negative annual income returns 0 deductions
         int result = taxCalculator.calculateTotalTaxDeductions(-4000000);
         assertEquals(0, result);
