9f757b604105212db619ca63fba290905345a9a6
Dan Buckland <danbucklan@gmail.com>
2015-09-28 21:30:56 +0100

    Add string values for value fields to constants file

--- features/support/constants.rb
+++ features/support/constants.rb
@@ -7,5 +7,10 @@ $fields = {
 }
 
 $strings = {
-  "Gross annual income" => "main_txt_gross_value"
+  "Gross annual income" => "main_txt_gross_value",
+  "Personal allowance" => "main_txt_allowance_value",
+  "Tax deductions" => "main_txt_tax_deductions_value",
+  "National Insurance contribution" => "main_txt_national_insurance_value",
+  "Total deductions" => "main_txt_total_deductions_value",
+  "Net annual income" => "main_txt_net_income_value"
 }
