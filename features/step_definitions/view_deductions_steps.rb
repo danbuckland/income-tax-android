b9798ded75eac77db725a474a6fddfc1cb9c116b
Dan Buckland <danbucklan@gmail.com>
2015-10-11 17:14:07 +0100

    Rename view_salary_deductions_steps to view_deductions_steps

--- features/step_definitions/view_deductions_steps.rb
+++ features/step_definitions/view_deductions_steps.rb
@@ -2,6 +2,10 @@ When(/^I enter an annual income of "([^\"]*)"$/) do |amount|
   @screens.home.set_annual_income(amount)
 end
 
+When(/^I choose to view a monthly breakdown$/) do
+  @screens.home.view_monthly
+end
+
 Then(/^I should see the following breakdown$/) do |table|
   @screens.home.check_deductions(table)
 end
